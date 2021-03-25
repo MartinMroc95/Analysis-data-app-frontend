adbaf6c9b803bba9b72ab506a11f9c16476a4156
Martin Mroc <martin.mroc@t-systems.com>
2021-03-25 14:53:52 +0100

    Changed Header, Navigation and Routing.

--- src/App.js
+++ src/App.js
@@ -1,47 +1,36 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Switch, Route, NavLink, useLocation } from 'react-router-dom'
 
-import clsx from 'clsx'
-import { makeStyles, useTheme } from '@material-ui/core/styles'
-import Drawer from '@material-ui/core/Drawer'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import List from '@material-ui/core/List'
-import CssBaseline from '@material-ui/core/CssBaseline'
-import Typography from '@material-ui/core/Typography'
-import Divider from '@material-ui/core/Divider'
-import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
 import ChevronRightIcon from '@material-ui/icons/ChevronRight'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemIcon from '@material-ui/core/ListItemIcon'
-import ListItemText from '@material-ui/core/ListItemText'
 import InsertChartOutlinedSharpIcon from '@material-ui/icons/InsertChartOutlinedSharp'
 import FolderOutlinedIcon from '@material-ui/icons/FolderOutlined'
 import DescriptionOutlinedIcon from '@material-ui/icons/DescriptionOutlined'
 
-import ExcelReader from './pages/ExcelReader'
+import { Container, Drawer, AppBar, Toolbar, List, CssBaseline, Typography, Divider, Box } from '@material-ui/core'
+import { Tooltip, IconButton, ListItem, ListItemIcon, ListItemText } from '@material-ui/core'
+
+import FileUploading from './pages/FileUploading'
 import FileManagement from './pages/FileManagement'
 import DataAnalysis from './pages/DataAnalysis'
 
+import { makeStyles, useTheme } from '@material-ui/core/styles'
+import clsx from 'clsx'
 import './App.css'
 
 const routes = [
   {
     path: '/',
     exact: true,
-    sidebar: () => <div>Loading files</div>,
-    main: () => <ExcelReader />,
+    main: () => <FileUploading />,
   },
   {
     path: '/file-management',
-    sidebar: () => <div>File management</div>,
     main: () => <FileManagement />,
   },
   {
     path: '/data-analysis',
-    sidebar: () => <div>Data Analysis</div>,
     main: () => <DataAnalysis />,
   },
 ]
@@ -101,105 +90,131 @@ const useStyles = makeStyles((theme) => ({
     alignItems: 'center',
     justifyContent: 'flex-end',
     padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
     ...theme.mixins.toolbar,
   },
   content: {
     flexGrow: 1,
     padding: theme.spacing(3),
   },
+  header: {
+    display: 'flex',
+    width: '100%',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  creator: { marginLeft: '5px', '&:hover': { color: '#000018', fontWeight: 'bold' } },
+  isActive: {
+    backgroundColor: '#3f51b5',
+    color: ' white',
+    '&:hover': {
+      backgroundColor: '#3f51b5',
+    },
+  },
+  isActiveIcon: {
+    color: 'white',
+  },
 }))
 
 const App = () => {
+  const location = useLocation()
   const classes = useStyles()
   const theme = useTheme()
-  const [open, setOpen] = React.useState(false)
 
-  const handleDrawerOpen = () => {
-    setOpen(true)
-  }
-
-  const handleDrawerClose = () => {
-    setOpen(false)
-  }
+  const [openBar, setOpenBar] = useState(false)
 
   return (
-    <Router>
-      <div className={classes.root}>
-        <CssBaseline />
-        <AppBar
-          position="fixed"
-          className={clsx(classes.appBar, {
-            [classes.appBarShift]: open,
-          })}
-        >
-          <Toolbar>
-            <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              onClick={handleDrawerOpen}
-              edge="start"
-              className={clsx(classes.menuButton, {
-                [classes.hide]: open,
-              })}
-            >
-              <MenuIcon />
-            </IconButton>
+    <Container maxWidth="xl" className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: openBar,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={() => setOpenBar(true)}
+            edge="start"
+            className={clsx(classes.menuButton, {
+              [classes.hide]: openBar,
+            })}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Box className={classes.header}>
             <Typography variant="h6" noWrap>
-              Mini variant drawer
+              Technological data analysis of the steelmaking
             </Typography>
-          </Toolbar>
-        </AppBar>
-        <Drawer
-          variant="permanent"
-          className={clsx(classes.drawer, {
-            [classes.drawerOpen]: open,
-            [classes.drawerClose]: !open,
-          })}
-          classes={{
-            paper: clsx({
-              [classes.drawerOpen]: open,
-              [classes.drawerClose]: !open,
-            }),
-          }}
-        >
-          <div className={classes.toolbar}>
-            <IconButton onClick={handleDrawerClose}>
-              {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-            </IconButton>
-          </div>
-          <Divider />
-          <List>
-            <ListItem button component={Link} to="/">
-              <ListItemIcon>
+            <Typography noWrap>
+              created by{' '}
+              <a className={classes.creator} target="_blank" rel="noreferrer" href="https://martinmroc.netlify.app/">
+                Martin Mroc
+              </a>
+            </Typography>
+          </Box>
+        </Toolbar>
+      </AppBar>
+
+      <Drawer
+        variant="permanent"
+        className={clsx(classes.drawer, {
+          [classes.drawerOpen]: openBar,
+          [classes.drawerClose]: !openBar,
+        })}
+        classes={{
+          paper: clsx({
+            [classes.drawerOpen]: openBar,
+            [classes.drawerClose]: !openBar,
+          }),
+        }}
+      >
+        <div className={classes.toolbar}>
+          <IconButton onClick={() => setOpenBar(false)}>
+            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          <ListItem activeClassName={classes.isActive} button exact component={NavLink} to={'/'}>
+            <Tooltip title="Loading files" placement="right" arrow>
+              <ListItemIcon className={location.pathname === '/' ? classes.isActiveIcon : null}>
                 <DescriptionOutlinedIcon />
               </ListItemIcon>
-              <ListItemText>Loading files</ListItemText>
-            </ListItem>
-            <ListItem button component={Link} to="/file-management">
-              <ListItemIcon>
+            </Tooltip>
+            <ListItemText>Loading files</ListItemText>
+          </ListItem>
+
+          <ListItem button activeClassName={classes.isActive} component={NavLink} to={'/file-management'}>
+            <Tooltip title="File management" placement="right" arrow>
+              <ListItemIcon className={location.pathname === '/file-management' ? classes.isActiveIcon : null}>
                 <FolderOutlinedIcon />
               </ListItemIcon>
-              <ListItemText>File management</ListItemText>
-            </ListItem>
-            <ListItem button component={Link} to="/data-analysis">
-              <ListItemIcon>
+            </Tooltip>
+            <ListItemText>File management</ListItemText>
+          </ListItem>
+
+          <ListItem button activeClassName={classes.isActive} component={NavLink} to={'/data-analysis'}>
+            <Tooltip title="Data analysis" placement="right" arrow>
+              <ListItemIcon className={location.pathname === '/data-analysis' ? classes.isActiveIcon : null}>
                 <InsertChartOutlinedSharpIcon />
               </ListItemIcon>
-              <ListItemText>Data Analysis</ListItemText>
-            </ListItem>
-          </List>
-        </Drawer>
-        <main className={classes.content}>
-          <div className={classes.toolbar} />
-          <Switch>
-            {routes.map((route, index) => (
-              <Route key={index} path={route.path} exact={route.exact} children={<route.main />} />
-            ))}
-          </Switch>
-        </main>
-      </div>
-    </Router>
+            </Tooltip>
+            <ListItemText>Data analysis</ListItemText>
+          </ListItem>
+        </List>
+      </Drawer>
+
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Switch>
+          {routes.map((route, index) => (
+            <Route key={index} path={route.path} exact={route.exact} children={<route.main />} />
+          ))}
+        </Switch>
+      </main>
+    </Container>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css'
 
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import { BrowserRouter as Router } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root')
+)
 reportWebVitals()
